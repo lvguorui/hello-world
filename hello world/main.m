33008e6811d1128700d01ed99a0f563269e098e2
lvguorui <lvguorui@admindeiMac-33.local>
2016-03-03 11:05:15 +0800

    Initial Commit

---
+++ hello world/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello world
+//
+//  Created by lvguorui on 16/3/3.
+//  Copyright (c) 2016年 lvguorui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
